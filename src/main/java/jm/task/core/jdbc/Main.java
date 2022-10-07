6f4b864543d454c4cffd2c11e429a317cc35ba57
Andrew <niramrus@gmail.com>
2022-10-07 20:18:14 +0500

    learning

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // реализуйте алгоритм здесь
+        Connection connection;
+        Util util = new Util();
+        connection = util.getConnection();
+        Statement statement = connection.createStatement();
+        String sql = "SELECT * FROM Usersmod";
+        ResultSet rs = statement.executeQuery(sql);
+        while (rs.next()) {
+            int uid = rs.getInt(1);
+            String uName = rs.getString(2);
+            String uLastName = rs.getString(3);
+            int age = rs.getByte(4);
+            System.out.printf("%d: %s, %s, возраст %d", uid, uName, uLastName, age);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        String hostName = "localhost";
+        String dbName = "mydatabase";
+        String userName = "root";
+        String password = "root";
+        return getConnection(hostName, dbName, userName, password);
+    }
+    public Connection getConnection(String hostname, String dbName, String userName, String password) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionURL = "jdbc:mysql://" + hostname + ":3306/" + dbName;
+        Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+        return conn;
+    }
 }
