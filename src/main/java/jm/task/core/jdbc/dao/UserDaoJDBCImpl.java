6188e3d180d0bb7462fc01c973dd0ea1cb36f742
Andrew <niramrus@gmail.com>
2022-10-10 20:08:19 +0500

    1.1.3, Statement -> PreparedStatement

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,24 +3,13 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private Statement statement;
     private List<User> listUser = new ArrayList<>();
 
-    {
-        try {
-            statement = getConn().createStatement();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     private Connection getConn() {
         Util util = new Util();
@@ -33,14 +22,15 @@ public class UserDaoJDBCImpl implements UserDao {
         return connection;
     }
 
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
         String sql = "CREATE TABLE if not exists Usersmod (id BIGINT UNSIGNED auto_increment, name VARCHAR(50), lastName VARCHAR(50), age TINYINT UNSIGNED, PRIMARY KEY (id));";
-        try {
-            statement.executeUpdate(sql);
+        try (PreparedStatement ps = getConn().prepareStatement(sql)) {
+            ps.executeUpdate(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -48,8 +38,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void dropUsersTable() {
         String sql = "DROP TABLE if exists Usersmod;";
-        try {
-            statement.executeUpdate(sql);
+        try (PreparedStatement ps = getConn().prepareStatement(sql)) {
+            ps.executeUpdate(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -57,8 +47,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT INTO Usersmod (name, lastName, age) VALUES ('" + name + "', '" + lastName + "', " + age + ");";
-        try {
-            statement.executeUpdate(sql);
+        try (PreparedStatement ps = getConn().prepareStatement(sql)) {
+            ps.executeUpdate(sql);
             System.out.println("User с именем – " + name + " " + lastName + " добавлен в базу данных");
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -67,8 +57,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void removeUserById(long id) {
         String sql = "DELETE FROM usersmod WHERE id = " + id + ";";
-        try {
-            statement.executeUpdate(sql);
+        try (PreparedStatement ps = getConn().prepareStatement(sql)) {
+            ps.executeUpdate(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -76,14 +66,13 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         String sql = "SELECT * FROM Usersmod";
-        ResultSet rs;
-        try {
-            rs = statement.executeQuery(sql);
-            while (rs.next()) {
-                long uid = rs.getLong(1);
-                String uName = rs.getString(2);
-                String uLastName = rs.getString(3);
-                byte age = rs.getByte(4);
+        try (PreparedStatement ps = getConn().prepareStatement(sql)) {
+            ps.executeQuery(sql);
+            while (ps.getResultSet().next()) {
+                long uid = ps.getResultSet().getLong(1);
+                String uName = ps.getResultSet().getString(2);
+                String uLastName = ps.getResultSet().getString(3);
+                byte age = ps.getResultSet().getByte(4);
                 User user = new User(uName, uLastName, age);
                 user.setId(uid);
                 listUser.add(user);
@@ -91,13 +80,14 @@ public class UserDaoJDBCImpl implements UserDao {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
         return listUser;
     }
 
     public void cleanUsersTable() {
         String sql = "DELETE FROM usersmod";
-        try {
-            statement.executeUpdate(sql);
+        try (PreparedStatement ps = getConn().prepareStatement(sql)) {
+            ps.executeUpdate(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
